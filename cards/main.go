b401d2d856d35669b670d37cadb5eef1ccd5d205
Mario Guerrero <mario.guerrero@payulatam.com>
2021-04-13 18:45:06 -0500

    mulple return type

--- cards/main.go
+++ cards/main.go
@@ -1,6 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
 	cards.print()
+	fmt.Println("---------")
+
+	hand, remainingDeck := deal(cards, 5)
+
+	hand.print()
+	fmt.Println("*********")
+	remainingDeck.print()
+
 }
