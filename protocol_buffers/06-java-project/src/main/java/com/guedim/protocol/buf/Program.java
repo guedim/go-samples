ddaf1b6da34f1104bd3ef1d2e73fe048a2d385e3
Mario Guerrero <mario.guerrero@payulatam.com>
2021-04-25 03:59:02 -0500

    is working

--- protocol_buffers/06-java-project/src/main/java/com/guedim/protocol/buf/Program.java
+++ protocol_buffers/06-java-project/src/main/java/com/guedim/protocol/buf/Program.java
@@ -1,13 +1,17 @@
 package com.guedim.protocol.buf;
 
-
 import example.simple.Simple;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Program {
     public static void main(String[] args)  {
 
         System.out.println("Hola Mundo");
-        Simple.SimpleMessage.Builder builder = SimpleMessage.newBuilder();
+        Simple.SimpleMessage.Builder builder = Simple.SimpleMessage.newBuilder();
         builder.setId(42)
                 .setIsSimple(true)
                 .setName("My simple message name")
@@ -16,7 +20,7 @@ public class Program {
                 .addSampleList(3);
 
         System.out.println(builder.toString());
-        SimpleMessage message = builder.build();
+        Simple.SimpleMessage message = builder.build();
 
         // write protocol to a file
         try {
@@ -34,7 +38,7 @@ public class Program {
         try {
             System.out.println("Reading from file ...");
             FileInputStream inputStream = new FileInputStream("simple-message.bin");
-            SimpleMessage messageFromFile =  SimpleMessage.parseFrom(inputStream);
+            Simple.SimpleMessage messageFromFile =  Simple.SimpleMessage.parseFrom(inputStream);
             System.out.println(messageFromFile.toString());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
