364a4ea5a08de8408b84de0eca32dd37d8ced151
Mario Guerrero <mario.guerrero@payulatam.com>
2021-04-26 00:00:47 -0500

    it is working  :D

--- protocol_buffers/07-go-project/main.go
+++ protocol_buffers/07-go-project/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/guedim/07-go-project/src/simple"
+)
 
 func main() {
 	fmt.Println("Hello World")
+	doSimple()
+}
+
+func doSimple() {
+	sm := simple.SimpleMessage{
+		Id:         12345,
+		IsSimple:   true,
+		Name:       "Mario",
+		SampleList: []int32{1, 2, 3, 4, 5},
+	}
+
+	fmt.Println(sm)
 }
