e03092d703d107a6a7210c05c02c5e41caee2da9
Mario Guerrero <mario.guerrero@payulatam.com>
2021-04-30 18:38:22 -0500

    server initiated succesfully

--- grpc-go-course/generate.sh
+++ grpc-go-course/generate.sh
@@ -2,4 +2,14 @@
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
 
+# gen code from protocol buffer file
 protoc greet/greetpb/greet.proto --go_out=plugins=grpc:.
+
+# init go module
+go mod init grpc-greeting 
+
+# look for dependencies
+go mod tidy 
+
+
+
