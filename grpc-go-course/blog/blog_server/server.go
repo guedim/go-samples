0a9e042167ea868fefb4609aa17a1c1fcd53e37e
Mario Guerrero <mario.guerrero@payulatam.com>
2021-05-10 22:48:28 -0500

    read blog server implementation

--- grpc-go-course/blog/blog_server/server.go
+++ grpc-go-course/blog/blog_server/server.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"os/signal"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -56,6 +57,29 @@ func (*server) CreateBlog(ctx context.Context, req *blogpb.CreateBlogRequest) (*
 		},
 	}, nil
 
+}
+
+func (*server) ReadBlog(ctx context.Context, req *blogpb.ReadBlogRequest) (*blogpb.ReadBlogResponse, error) {
+	fmt.Println("Read blog request !!!")
+	blogId := req.GetBlogId()
+	oid, err := primitive.ObjectIDFromHex(blogId)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, fmt.Sprintf("cannot parse Id : %v", err))
+	}
+
+	// create an empty struct
+	data := &blogItem{}
+	filter := bson.NewDocument(bson.EC.ObjectID("_id", oid))
+	res := collection.FindOne(context.Background(), filter)
+	if err := res.Decode(data); err != nil {
+		return nil, status.Errorf(codes.NotFound, fmt.Sprintf("Cannot find blog with specified id: %v", err))
+	}
+	return &blogpb.ReadBlogResponse{Blog: &blogpb.Blog{
+		Id:       data.ID.Hex(),
+		AuthorId: data.AuthorID,
+		Content:  data.Content,
+		Title:    data.Title,
+	}}, nil
 }
 
 func main() {
